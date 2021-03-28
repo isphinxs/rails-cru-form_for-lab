da38a85d66f9cea3944570c72a79916d1ef581ae
Samantha Ostrowski <isphinxs@hotmail.com>
2021-03-28 14:13:06 -0400

    commit tree bebadad3a5528b2554c2a8e8ee3909550ec2b104

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,36 @@
+class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    find_artist
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(params.require(:artist).permit(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    find_artist
+  end
+
+  def update
+    find_artist
+    @artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  private
+
+  def find_artist
+    @artist = Artist.find_by(id: params[:id])
+  end
+
+end
